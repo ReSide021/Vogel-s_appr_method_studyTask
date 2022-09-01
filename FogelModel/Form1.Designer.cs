797ec80281c8813b57b21d2e6074988156f9048d
RSide021 <danila.sizih@gmail.com>
2022-09-01 22:36:02 +0800

    Uploading a project

---
+++ FogelModel/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace FogelModel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownClmn = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownRow = new System.Windows.Forms.NumericUpDown();
+            this.button_ok = new System.Windows.Forms.Button();
+            this.textBox_info = new System.Windows.Forms.TextBox();
+            this.button_solve = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownClmn)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.AllowUserToAddRows = false;
+            this.dataGridView.AllowUserToDeleteRows = false;
+            this.dataGridView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Location = new System.Drawing.Point(12, 86);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowHeadersWidth = 51;
+            this.dataGridView.RowTemplate.Height = 24;
+            this.dataGridView.Size = new System.Drawing.Size(1364, 389);
+            this.dataGridView.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Столбцы (заказчики):";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(334, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(161, 24);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Строки (запасы):";
+            // 
+            // numericUpDownClmn
+            // 
+            this.numericUpDownClmn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownClmn.Location = new System.Drawing.Point(225, 35);
+            this.numericUpDownClmn.Name = "numericUpDownClmn";
+            this.numericUpDownClmn.Size = new System.Drawing.Size(73, 28);
+            this.numericUpDownClmn.TabIndex = 3;
+            // 
+            // numericUpDownRow
+            // 
+            this.numericUpDownRow.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDownRow.Location = new System.Drawing.Point(501, 35);
+            this.numericUpDownRow.Name = "numericUpDownRow";
+            this.numericUpDownRow.Size = new System.Drawing.Size(73, 28);
+            this.numericUpDownRow.TabIndex = 4;
+            // 
+            // button_ok
+            // 
+            this.button_ok.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_ok.Location = new System.Drawing.Point(629, 24);
+            this.button_ok.Name = "button_ok";
+            this.button_ok.Size = new System.Drawing.Size(89, 50);
+            this.button_ok.TabIndex = 5;
+            this.button_ok.Text = "OK";
+            this.button_ok.UseVisualStyleBackColor = true;
+            this.button_ok.Click += new System.EventHandler(this.button_ok_Click);
+            // 
+            // textBox_info
+            // 
+            this.textBox_info.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox_info.Location = new System.Drawing.Point(12, 481);
+            this.textBox_info.Multiline = true;
+            this.textBox_info.Name = "textBox_info";
+            this.textBox_info.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox_info.Size = new System.Drawing.Size(706, 338);
+            this.textBox_info.TabIndex = 6;
+            // 
+            // button_solve
+            // 
+            this.button_solve.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button_solve.Location = new System.Drawing.Point(741, 24);
+            this.button_solve.Name = "button_solve";
+            this.button_solve.Size = new System.Drawing.Size(157, 50);
+            this.button_solve.TabIndex = 7;
+            this.button_solve.Text = "Рассчитать";
+            this.button_solve.UseVisualStyleBackColor = true;
+            this.button_solve.Click += new System.EventHandler(this.button_solve_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1388, 831);
+            this.Controls.Add(this.button_solve);
+            this.Controls.Add(this.textBox_info);
+            this.Controls.Add(this.button_ok);
+            this.Controls.Add(this.numericUpDownRow);
+            this.Controls.Add(this.numericUpDownClmn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView);
+            this.Name = "Form1";
+            this.Text = "Метод Фогеля";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownClmn)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownRow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownClmn;
+        private System.Windows.Forms.NumericUpDown numericUpDownRow;
+        private System.Windows.Forms.Button button_ok;
+        private System.Windows.Forms.TextBox textBox_info;
+        private System.Windows.Forms.Button button_solve;
+    }
+}
+
---
+++ FogelModel/Form1.cs
@@ -0,0 +1,424 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FogelModel
+{
+    public partial class Form1 : Form
+    {
+        int countClmn = 0;
+        int countRow = 0;
+        List<int> endSum = new List<int>();
+        bool endScore = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_ok_Click(object sender, EventArgs e)
+        {
+            textBox_info.Clear();
+            endScore = false;
+            endSum.Clear();
+            countClmn = (int)numericUpDownClmn.Value;
+            countRow = (int)numericUpDownRow.Value;
+            if (countClmn == 0 || countRow == 0) return;
+            while(dataGridView.Rows.Count > 0)
+            {
+                dataGridView.Rows.Clear();
+            }
+            while(dataGridView.Columns.Count > 0)
+            {
+                dataGridView.Columns.Clear();
+            }
+            for(int i = 0; i <= numericUpDownClmn.Value; i++)
+            {
+                if(i == 0)
+                {
+                    dataGridView.Columns.Add($"clmn{i}", "");
+                }
+                else
+                {
+                    dataGridView.Columns.Add($"clmn{i}", $"Заказчик {i}");
+                }
+            }
+            for (int i = 0; i <= numericUpDownRow.Value; i++)
+            {
+                dataGridView.Rows.Add();
+            }
+            dataGridView[0, 0].Value = "Запасы\\Требуется";
+        }
+
+        private async void button_solve_Click(object sender, EventArgs e)
+        {
+            int stocks = 0;
+            for(int i = 1; i <= numericUpDownRow.Value; i++)
+            {
+                stocks += Convert.ToInt32(dataGridView[0, i].Value);
+            }
+            int wanted = 0;
+            for(int i = 1; i <= numericUpDownClmn.Value; i++)
+            {
+                wanted += Convert.ToInt32(dataGridView[i, 0].Value);
+            }
+            //seekMaxinMin();
+            if (stocks != wanted)
+            {
+                MessageBox.Show("Задача не является закрытым типом");
+            }
+            else
+            {
+                while (!endScore)
+                {
+                    await Task.Delay(1000);
+                    await seekMaxinMin();
+                }
+            }
+        }
+
+
+        private void PrintEndSum(List<int> endSum)
+        {
+            int summa = 0;
+            foreach(var el in endSum)
+            {
+                summa += el;
+            }
+            textBox_info.Text += $"\r\n\r\nИТОГОВАЯ СУММА: {summa}\r\n\r\n";
+        }
+
+        private async Task<int> seekMaxinMin()
+        {
+            textBox_info.Text += $"\r\n\r\n____________Запуск новой итерации____________\r\n\r\n";
+            // создание новых столбца и строки для минимумов
+            var newClmn = dataGridView.Columns.Count;
+            var newRow = dataGridView.Rows.Count;
+            dataGridView.Columns.Add($"clmn{newClmn}", "Разность");
+            dataGridView.Rows.Add();
+            dataGridView[0, newRow].Value = "Разность";
+
+            List<int> listDiffNum = new List<int>();
+
+            // заполение новой строки
+            for(int i = 1; i <= countClmn; i++)
+            {
+                List<int> numInClmn = new List<int>();
+                bool end = false;
+                for(int j = 1; j <= countRow; j++)
+                {
+                    if(dataGridView[i, j].Style.BackColor == Color.Empty)
+                    {
+                        var num = Convert.ToInt32(dataGridView[i, j].Value);
+                        numInClmn.Add(num);
+                       // textBox_info.Text += $"Строка {j} зафиксирована\r\n";
+                    }
+                    if (j == countRow) end = true;
+                }
+                if(end)
+                {
+                    if(numInClmn.Count > 1)
+                    {
+                        var min1 = numInClmn.Min();
+                        numInClmn.Remove(min1);
+                        var min2 = numInClmn.Min();
+                        var res = min2 - min1;
+                        dataGridView[i, newRow].Value = res;
+                        listDiffNum.Add(res);
+                        textBox_info.Text += $"Разность минимальных значений в столбце {i} = {res}\r\n";
+                    } 
+                    else if(numInClmn.Count == 1)
+                    {
+                        for (int j = 1; j <= countRow; j++)
+                        {
+                            if (dataGridView[i, j].Style.BackColor == Color.Empty)
+                            {
+                                var num = Convert.ToInt32(dataGridView[i, j].Value);
+                                var store = Convert.ToInt32(dataGridView[0, j].Value);
+                                var needed = Convert.ToInt32(dataGridView[i, 0].Value);
+                                if (store <= needed)
+                                {
+                                    int res = num * store;
+                                    endSum.Add(res);
+                                    textBox_info.Text += $"Добавлено итоговое число {res}\r\n";
+                                    await Task.Delay(1500);
+                                    dataGridView[0, j].Value = "0";
+                                    dataGridView[0, j].Style.ForeColor = Color.Yellow;
+                                    dataGridView[i, j].Style.BackColor = Color.LightPink;
+                                    dataGridView[i, 0].Value = needed - store;
+                                    if(store == needed)
+                                    {
+                                        dataGridView[i, 0].Style.ForeColor = Color.Green;
+                                    }
+                                }
+                                else
+                                {
+                                    int res = num * needed;
+                                    endSum.Add(res);
+                                    textBox_info.Text += $"Добавлено итоговое число {res}\r\n";
+                                    await Task.Delay(1500);
+                                    dataGridView[0, j].Value = store - needed;
+                                    dataGridView[i, 0].Style.ForeColor = Color.Green;
+                                    dataGridView[i, j].Style.BackColor = Color.LightPink;
+                                }
+                                endScore = true;
+                                PrintEndSum(endSum);
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            // заполнение нового столбца
+            for(int i = 1; i <= countRow; i++)
+            {
+                List<int> numInRow = new List<int>();
+                bool end = false;
+                for (int j = 1; j <= countClmn; j++)
+                {
+                    if(dataGridView[j, i].Style.BackColor == Color.Empty)
+                    {
+                        var num = Convert.ToInt32(dataGridView[j, i].Value);
+                        numInRow.Add(num);
+                       // textBox_info.Text += $"Столбец {j} зафиксирован\r\n";
+                    }
+                    if (j == countClmn) end = true;
+                }
+                if(end)
+                {
+                    if(numInRow.Count > 1)
+                    {
+                        var min1 = numInRow.Min();
+                        numInRow.Remove(min1);
+                        var min2 = numInRow.Min();
+                        var res = min2 - min1;
+                        dataGridView[newClmn, i].Value = res;
+                        listDiffNum.Add(res);
+                        textBox_info.Text += $"Разность минимальных значений в строке {i} = {res}\r\n";
+                    }
+                    else if(numInRow.Count == 1)
+                    {
+                        for (int j = 1; j <= countClmn; j++)
+                        {
+                            if (dataGridView[j, i].Style.BackColor == Color.Empty)
+                            {
+                                var num = Convert.ToInt32(dataGridView[j, i].Value);
+                                var store = Convert.ToInt32(dataGridView[0, i].Value);
+                                var needed = Convert.ToInt32(dataGridView[j, 0].Value);
+                                if(store <= needed)
+                                {
+                                    int res = num * store;
+                                    endSum.Add(res);
+                                    textBox_info.Text += $"Добавлено итоговое число {res}\r\n";
+                                    await Task.Delay(1500);
+                                    dataGridView[0, i].Value = "0";
+                                    dataGridView[0, i].Style.ForeColor = Color.Yellow;
+                                    dataGridView[j, i].Style.BackColor = Color.LightPink;
+                                    dataGridView[j, 0].Value = needed - store;
+                                    if(store == needed)
+                                    {
+                                        dataGridView[j, 0].Style.ForeColor = Color.Green;
+                                    }
+                                }
+                                else
+                                {
+                                    int res = num * needed;
+                                    endSum.Add(res);
+                                    textBox_info.Text += $"Добавлено итоговое число {res}\r\n";
+                                    await Task.Delay(1500);
+                                    dataGridView[0, i].Value = store - needed;
+                                    dataGridView[j, 0].Style.ForeColor = Color.Green;
+                                    dataGridView[j, i].Style.BackColor = Color.LightPink;
+                                }
+                                endScore = true;
+                                PrintEndSum(endSum);
+                            }
+                        }
+                        
+                    }
+                }
+            }
+
+            if (endScore)
+            {
+                dataGridView.Rows.RemoveAt(newRow);
+                dataGridView.Columns.RemoveAt(newClmn);
+                return 0;
+            }
+
+            for (int i = 1; i <= countRow; i++)
+            {
+                if (dataGridView[newClmn, i].Style.BackColor == Color.LightPink) continue;
+                if (Convert.ToInt32(dataGridView[newClmn, i].Value) == listDiffNum.Max())
+                {
+                    textBox_info.Text += $"Найден максимум в столбце разностей [{newClmn},{i}]\r\n";
+                    dataGridView[newClmn, i].Style.BackColor = Color.LightGreen;
+                    textBox_info.Text += $"Запуск поиска минимума в строке {i}\r\n";
+                    seekRowOrClmn(true, i);
+                    return 0;
+                }
+            }
+            for (int i = 1; i <= countClmn; i++)
+            {
+                if (dataGridView[i, newRow].Style.BackColor == Color.LightPink) continue;
+                if (Convert.ToInt32(dataGridView[i, newRow].Value) == listDiffNum.Max())
+                {
+                    textBox_info.Text += $"Найден максимум в строке разностей [{i},{newRow}]\r\n";
+                    dataGridView[i, newRow].Style.BackColor = Color.LightGreen;
+                    textBox_info.Text += $"Запуск поиска минимума в столбце {i}\r\n";
+                    seekRowOrClmn(false, i);
+                    return 0;
+                }
+            }
+            return 0;
+        }
+
+        private void seekRowOrClmn(bool IsSeekRow, int coord)
+        {
+            if (IsSeekRow) // максимум в столбце справа - ищем строку
+            {
+                List<int> tempList = new List<int>();
+                for(int i = 1; i <= countClmn; i++)
+                {
+                    if (dataGridView[i, coord].Style.BackColor == Color.LightPink) continue;
+                    var num = Convert.ToInt32(dataGridView[i, coord].Value);
+                    tempList.Add(num);
+                    //textBox_info.Text += $"В tempList добавлено число {num} [{i}, {coord}]\r\n";
+                }
+                int saveI = 0;
+                string type = "";
+                for (int i = 1; i <= countClmn; i++)
+                {
+                    if (dataGridView[i, coord].Style.BackColor == Color.LightPink) continue;
+                    if(Convert.ToInt32(dataGridView[i, coord].Value) == tempList.Min())
+                    {
+                        var num = Convert.ToInt32(dataGridView[i, coord].Value);
+                        var store = Convert.ToInt32(dataGridView[0, coord].Value);
+                        var needed = Convert.ToInt32(dataGridView[i, 0].Value);
+                        if(needed <= store)
+                        {
+                            int res = num * needed;
+                            endSum.Add(res);
+                            textBox_info.Text += $"Добавлено итоговое число {res}\r\n";
+                            dataGridView[0, coord].Value = store - needed;
+                            dataGridView[i, 0].Style.ForeColor = Color.Green;
+                            dataGridView[i, 0].Value = "0";
+                            saveI = i;
+                            type = "fullneeded";
+                        }
+                        else
+                        {
+                            int res = num * store;
+                            endSum.Add(res);
+                            textBox_info.Text += $"Добавлено итоговое число {res}\r\n";
+                            dataGridView[0, coord].Value = "0";
+                            dataGridView[0, coord].Style.ForeColor = Color.Yellow;
+                            dataGridView[i, 0].Value = needed - store;
+                            type = "nonfullneeded";
+                        }
+                        PrintEndSum(endSum);
+                        break;
+                    }
+                }
+                if(type == "fullneeded")
+                {
+                    if (saveI != 0)
+                    {
+                        textBox_info.Text += $"Номер закрытого столбца: {saveI}\r\n";
+                        for (int i = 1; i <= countRow; i++)
+                        {
+                            dataGridView[saveI, i].Style.BackColor = Color.LightPink;
+                           // textBox_info.Text += $"Закрыта секция (столбец) [{saveI},{i}]\r\n";
+                        }
+                    }
+                }
+                if(type == "nonfullneeded")
+                {
+                    if (dataGridView[0, coord].Value.ToString() == "0")
+                    {
+                        textBox_info.Text += $"Номер закрытой строки: {coord}\r\n";
+                        for (int i = 1; i <= countClmn; i++)
+                        {
+                            dataGridView[i, coord].Style.BackColor = Color.LightPink;
+                            //textBox_info.Text += $"Закрыта секция (строка) [{i},{coord}]\r\n";
+                        }
+                    }
+                }
+            }
+            else
+            {
+                List<int> tempList = new List<int>();
+                for (int i = 1; i <= countRow; i++)
+                {
+                    if (dataGridView[coord, i].Style.BackColor == Color.LightPink) continue;
+                    var num = Convert.ToInt32(dataGridView[coord, i].Value);
+                    tempList.Add(num);
+                    //textBox_info.Text += $"В tempList добавлено число {num} [{coord}, {i}]\r\n";
+                }
+                int saveI = 0;
+                string type = "";
+                for (int i = 1; i <= countRow; i++)
+                {
+                    if (dataGridView[coord, i].Style.BackColor == Color.LightPink) continue;
+                    if (Convert.ToInt32(dataGridView[coord, i].Value) == tempList.Min())
+                    {
+                        var num = Convert.ToInt32(dataGridView[coord, i].Value);
+                        var store = Convert.ToInt32(dataGridView[0, i].Value);
+                        var needed = Convert.ToInt32(dataGridView[coord, 0].Value);
+                        if (needed <= store)
+                        {
+                            int res = num * needed;
+                            endSum.Add(res);
+                            textBox_info.Text += $"Добавлено итоговое число {res}\r\n";
+                            dataGridView[0, i].Value = store - needed;
+                            dataGridView[coord, 0].Style.ForeColor = Color.Green;
+                            dataGridView[coord, 0].Value = "0";
+                            saveI = i;
+                            type = "fullneeded";
+                        }
+                        else
+                        {
+                            int res = num * store;
+                            endSum.Add(res);
+                            textBox_info.Text += $"Добавлено итоговое число {res}\r\n";
+                            dataGridView[0, i].Value = "0";
+                            dataGridView[0, i].Style.ForeColor = Color.Yellow;
+                            dataGridView[coord, 0].Value = needed - store;
+                            saveI = i;
+                            type = "nonfullneeded";
+                        }
+                        PrintEndSum(endSum);
+                        break;
+                    }
+                }
+                if(type == "nonfullneeded")
+                {
+                    if (saveI != 0)
+                    {
+                        textBox_info.Text += $"Номер закрытой строки: {saveI}\r\n";
+                        if (dataGridView[0, saveI].Value.ToString() == "0")
+                        {
+                            for (int i = 1; i <= countClmn; i++)
+                            {
+                                dataGridView[i, saveI].Style.BackColor = Color.LightPink;
+                                textBox_info.Text += $"Закрыта секция (строка) [{i},{coord}]\r\n";
+                            }
+                        }
+                    }
+                }
+                if(type == "fullneeded")
+                {
+                    textBox_info.Text += $"Номер закрытого столбца: {coord}\r\n";
+                    for (int i = 1; i <= countRow; i++)
+                    {
+                        dataGridView[coord, i].Style.BackColor = Color.LightPink;
+                        textBox_info.Text += $"Закрыта секция (столбец) [{saveI},{i}]\r\n";
+                    }
+                }
+            }
+        }
+    }
+}
